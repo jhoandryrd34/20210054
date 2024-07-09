919a0d13140fdac73faedb0cb1c7e42e2df790d4
jhoandryrd34 <jhoandryrd34@gmail.com>
2024-07-08 20:23:28 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -23,5 +23,16 @@ const App = () => {
     </div>
   )
 }
+const App = () => {
+  // const-definitions
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content ... />
+      <Total ... />
+    </div>
+  )
+}
 
 export default App
